75a49e041f3c1a802ddab752d81ce31dafaaf654
Danny List <daniel.list@spareroom.co.uk>
2026-04-18 07:13:21 +0100

    v1.1.0
    Adjusted code to account for latest changes.
    - Event listeners on the unstack button and the per page dropdown.
    - Code clean up.

--- quickstock-restyler.user.js
+++ quickstock-restyler.user.js
@@ -1,22 +1,57 @@
 // ==UserScript==
 // @name         Neopets Quickstock Style Adjuster
-// @version      v1.0.0
-// @description  Adjusts the styles on the new Neopets Quickstock page to make it easier to use. Currently only reduces padding, but other adjustments may follow.
+// @version      v1.1.0
+// @description  Modifies the styles on the latest Neopets Quickstock page to make it easier to use. 
 // @author       Danny [amulthate]
 // @match        https://www.neopets.com/quickstock.phtml*
 // ==/UserScript==
 
 (function() {
     'use strict';
-    const headerTitles = document.querySelectorAll('.quickstock-table.np-table thead th');
+
+    const headerSelector = '.quickstock-table.np-table thead th';
+    const itemSelector = '.quickstock-table.np-table tbody td';
+    const unstackIconSelector = '.unstack-icon';
+    const perPageDropdownSelector = '#qs-per-page-select';
+
+    const headerTitles = document.querySelectorAll(headerSelector);
+    const items = document.querySelectorAll(itemSelector);
+    const unstackIcon = document.querySelector(unstackIconSelector);
+    const perPageDropdown = document.querySelector(perPageDropdownSelector);
+
+    function styleItemRows(items) {
+        items.forEach((item) => {
+            item.style.padding = "5px";
+        });
+    }
+
     headerTitles.forEach((headerTitle) => {
         headerTitle.style.padding = "5px";
         headerTitle.style.fontFamily = "Museo Sans Rounded";
         headerTitle.style.fontSize = "16px";
     });
 
-    const items = document.querySelectorAll('.quickstock-table.np-table tbody td');
     items.forEach((item) => {
         item.style.padding = "5px";
     });
+
+    if (unstackIcon) {
+        unstackIcon.addEventListener('click', () => {
+            setTimeout(() => {
+                const newItems = document.querySelectorAll(itemSelector);
+                styleItemRows(newItems);
+            }, 100);
+        });
+    }
+
+    if (perPageDropdown) {
+        perPageDropdown.addEventListener('change', () => {
+            setTimeout(() => {
+                const newItems = document.querySelectorAll(itemSelector);
+                newItems.forEach((item) => {
+                    styleItemRows(newItems);
+                });
+            }, 100);
+        });
+    }
 })();
