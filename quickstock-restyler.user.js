95ea36389444a4a9ef03088773a2ee811999bf05
Danny List <daniel.list@spareroom.co.uk>
2026-05-20 08:26:38 +0100

    v1.2.0
    - Fix bugs with new version of the page.

--- quickstock-restyler.user.js
+++ quickstock-restyler.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Neopets Quickstock Style Adjuster
-// @version      v1.1.0
+// @version      v1.2.0
 // @description  Modifies the styles on the latest Neopets Quickstock page to make it easier to use.
 // @author       Danny
 // @match        https://www.neopets.com/quickstock.phtml*
@@ -9,17 +9,23 @@
 (function() {
     'use strict';
 
-    const timeout = 1000;
+    const timeout = 1500;
     const headerSelector = '.quickstock-table thead tr th';
     const itemSelector = '.quickstock-table tbody tr td';
     const unstackIconSelector = '.unstack-icon';
     const perPageDropdownSelector = '#qs-per-page-select';
 
     setTimeout(() => {
-        const headerTitles = document.querySelectorAll(headerSelector);
-        const items = document.querySelectorAll(itemSelector);
         const unstackIcon = document.querySelector(unstackIconSelector);
         const perPageDropdown = document.querySelector(perPageDropdownSelector);
+
+        function getHeaderTitles() {
+            return document.querySelectorAll(headerSelector);
+        }
+
+        function getItems() {
+            return document.querySelectorAll(itemSelector);
+        }
 
         function styleItemRows(items) {
             items.forEach((item) => {
@@ -27,11 +33,18 @@
             });
         }
 
-        headerTitles.forEach((headerTitle) => {
-            headerTitle.style.padding = "5px";
-            headerTitle.style.fontFamily = "Museo Sans Rounded";
-            headerTitle.style.fontSize = "16px";
-        });
+        function styleHeaderRow(headerTitles) {
+            headerTitles.forEach((headerTitle) => {
+                headerTitle.style.padding = "5px";
+                headerTitle.style.fontFamily = "Museo Sans Rounded";
+                headerTitle.style.fontSize = "16px";
+            });
+        }
+
+        const headerTitles = getHeaderTitles();
+        const items = getItems();
+
+        styleHeaderRow(headerTitles);
 
         items.forEach((item) => {
             item.style.padding = "5px";
@@ -41,18 +54,20 @@
             unstackIcon.addEventListener('click', () => {
                 setTimeout(() => {
                     const newItems = document.querySelectorAll(itemSelector);
+                    const newHeaderTitles = document.querySelectorAll(headerSelector);
+                    styleHeaderRow(newHeaderTitles);
                     styleItemRows(newItems);
-                }, 100);
+                }, timeout);
             });
         }
 
         if (perPageDropdown) {
             perPageDropdown.addEventListener('change', () => {
                 setTimeout(() => {
-                    const newItems = document.querySelectorAll(itemSelector);
-                    newItems.forEach((item) => {
-                        styleItemRows(newItems);
-                    });
+                    const newPageItems = getItems();
+                    const newPageHeaderTitles = getHeaderTitles();
+                    styleHeaderRow(newPageHeaderTitles);
+                    styleItemRows(newPageItems);
                 }, timeout);
             });
         }
