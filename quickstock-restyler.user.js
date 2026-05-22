5b5cb194be95b9a027de4eb9852f4627022612d3
Danny List <daniel.list@spareroom.co.uk>
2026-05-22 08:00:56 +0100

    Increase version number

--- quickstock-restyler.user.js
+++ quickstock-restyler.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Neopets Quickstock Style Adjuster
-// @version      v1.2.0
+// @version      v2.0   
 // @description  Modifies the styles on the latest Neopets Quickstock page to make it easier to use.
 // @author       Danny
 // @match        https://www.neopets.com/quickstock.phtml*
